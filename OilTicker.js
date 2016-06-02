4ed9fd27d5be27ddc93d8b16c6a03bd568eb4603
johsund <jsn@qlik.com>
2016-06-02 10:42:39 +0800

    URL update

--- OilTicker.js
+++ OilTicker.js
@@ -32,14 +32,8 @@ define( ["jquery", "text!./style.css", "./js/tv"], function ( $, cssContent, tv 
 				$("#" + id).empty();
 			}
 			else {
-				$element.append($('<iframe src="file:///C:/Users/jsn/Documents/Qlik/Sense/Extensions/OilTicker/tickerWidget.html" style="border: 0;overflow:hidden;"/>').attr("id", id).width(width).height(height));
+				$element.append($('<iframe src="https://cdn.rawgit.com/johsund/OilTicker/master/tickerWidget.html" style="border: 0;overflow:hidden;"/>').attr("id", id).width(width).height(height));
 			}
-		
-			//$("#"+id).append('<div id="iframediv" style="height:100%;width:100%;"></div>');
-
-
-		
-		//$("#"+id).append('<script type="text/javascript" src="https://d33t3vvu2t2yu5.cloudfront.net/tv.js"></script><script type="text/javascript">new TradingView.widget({"width": '+width+',"height":' +height+ ',"symbol": "CLN16","interval": "1","timezone": "Etc/UTC","theme": "White","style": "1","locale": "en","toolbar_bg": "#f1f3f6","hide_top_toolbar": true,"save_image": false,"hideideas": true});</script>');
 		}
 	};
 } );
