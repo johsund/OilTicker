2aa4406639362e7aab9db1862bfd5b866ce58b83
johsund <jsn@qlik.com>
2016-06-08 13:13:27 +0800

    Fixed redraw issue

--- OilTicker.js
+++ OilTicker.js
@@ -1,5 +1,5 @@
 /*globals define*/
-define( ["jquery", "text!./style.css", "./js/tv"], function ( $, cssContent, tv ) {
+define( ["jquery", "text!./style.css"], function ( $, cssContent ) {
 	'use strict';
 	$( "<style>" ).html( cssContent ).appendTo( "head" );
 	return {
@@ -25,15 +25,19 @@ define( ["jquery", "text!./style.css", "./js/tv"], function ( $, cssContent, tv 
 		paint: function ( $element, layout ) {
 		  var width = $element.width();
 		  var height = $element.height();
-		  
+
+		  console.log("Paint triggered");
 		  var id = "container_" + layout.qInfo.qId;
 			
 			if(document.getElementById(id)) {
 				$("#" + id).empty();
 			}
 			else {
-				$element.append($('<iframe src="https://cdn.rawgit.com/johsund/OilTicker/19afa2037e2e41c43d2f88137e7689d9107d6b93/tickerWidget.html" style="border: 0;overflow:hidden;"/>').attr("id", id).width(width).height(height));
+				$element.append($('<div />').attr("id", id).width(width).height(height));				
 			}
+
+				$("#" + id).append($('<iframe src="https://cdn.rawgit.com/johsund/OilTicker/19afa2037e2e41c43d2f88137e7689d9107d6b93/tickerWidget.html" style="border: 0;overflow:hidden;"/>').width(width).height(height));
+			
 		}
 	};
 } );
